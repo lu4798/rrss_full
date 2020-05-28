eb12b25143beffee9be092ee84ebf727f9f0df00
Lucía Montesinos <lucia.montesinos@live.u-tad.com>
2020-05-28 19:34:28 +0200

    chat basic

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,51 +1,18 @@
-import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("intenta conectar")
         user = await self.scope['user_task']
         self.scope['user'] = user
-        print('scope in connect ', self.scope)
-        self.room_name = 'general'
-        self.room_group_name = 'chat_general'
-        print(self.channel_name)
-
 
         if user.is_anonymous:
-            pass
-            #await self.close()
+            await self.close()
         else:
-            await self.channel_layer.group_add(
-                self.room_group_name,
-                self.channel_name
-            )
             await self.accept()
 
 
     async def receive(self, text_data=None, bytes_data=None):
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'message',
-                'message': text_data,
-                'username': self.scope['user'].username
-            }
-        )
-
-
-    async def message(self, event):
-        await self.send(text_data=json.dumps({
-            'message': event['message'],
-            'username': event['username']
-        }))
-
+        await self.send(text_data)
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        pass
