133ca3b367bdc3fa2d3545d46a9cbe859e154bc1
Lucía Montesinos <lucia.montesinos@live.u-tad.com>
2020-06-02 22:14:04 +0200

    chat privado 3

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -10,7 +10,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         #self.room_name = 'general'
         #self.room_group_name = 'chat_general'
         print(self.scope['query_string'])
-        self.room_name = self.scope['query_string']['room_name']
+        #self.room_name = self.scope['query_string']['room_name']
+        self.room_name = self.scope['query_string'].decode('utf-8')
         self.room_group_name = 'chat%s' % self.room_name
         print(self.channel_name)
 
